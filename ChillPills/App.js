12280bd222491fb3aae50d93f597405cacf58504
T-Wan-Lin <tanwanlin.c2021@gmail.com>
2022-05-22 15:57:04 +0800

    debugging

--- ChillPills/App.js
+++ ChillPills/App.js
@@ -1,46 +1,42 @@
-import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import {createStackNavigator } from '@react-navigation/stack';
+import React, {useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import onBoardingScreen from './screens/onBoardingScreen';
 import loginScreen from './screens/loginScreen';
-import { useEffect } from 'react/cjs/react.production.min';
-import AsyncStorage, { AsyncStorageHook } from '@react-native-async-storage/async-storage';
+import {useEffect} from 'react';
+import AsyncStorage, {
+  AsyncStorageHook,
+} from '@react-native-async-storage/async-storage';
 
 const AppStack = createStackNavigator();
 const App = () => {
-  const [isFirstLaunch, setIsFirstLaunch] = useState(null); //is this what's causing the hook error?
+  const isFirstLaunch = true; //is this what's causing the hook error?
 
   useEffect(() => {
     AsyncStorage.getItem('alreadyLaunched').then(value => {
-      if(value == null) {
+      if (value == null) {
         AsyncStorage.setItem('alreadyLaunched', 'true');
-        setIsFirstLaunch(true);
+        //setIsFirstLaunch(true);
       } else {
-        setIsFirstLaunch(false);
+        //setIsFirstLaunch(false);
       }
-
     });
   }, []);
-  if(isFirstLaunch=== null) {
+  if (isFirstLaunch === null) {
     return null;
-  } else if(isFirstLaunch === true)
-  {
+  } else if (isFirstLaunch === true) {
     return (
       <NavigationContainer>
-        <AppStack.Navigator 
-          headerMode = "none"
-        >
-          <AppStack.Screen name = "onBoarding" component ={onBoardingScreen} />
-          <AppStack.Screen name = "Login" component ={loginScreen} /> 
+        <AppStack.Navigator headerShown="false">
+          <AppStack.Screen name="onBoarding" component={onBoardingScreen} />
+          <AppStack.Screen name="Login" component={loginScreen} />
         </AppStack.Navigator>
       </NavigationContainer>
-    ); 
+    );
+  } else {
+    <loginScreen />;
   }
-  else {
-    <loginScreen />
-  }
-  
-}
+};
 
 export default App;
--- ChillPills/screens/loginScreen.js
+++ ChillPills/screens/loginScreen.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import {View, Text, Button, StyleSheet, Image} from 'react-native';
 
 const loginScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text>LoginScreen</Text>
-            <Button
-                title="Click me"
-                onPress = {()=>alert('Button Clicked!')}
-                />
-
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>LoginScreen</Text>
+      <Button title="Click me" onPress={() => alert('Button Clicked!')} />
+    </View>
+  );
 };
 
 export default loginScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
