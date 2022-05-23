e58e46aaf860193875ed071c2b25e8b3f45f1501
T-Wan-Lin <tanwanlin.c2021@gmail.com>
2022-05-23 16:14:43 +0800

    login UI

--- ChillPills/screens/loginScreen.js
+++ ChillPills/screens/loginScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet, Image} from 'react-native';
+import {View, Text, Button, StyleSheet, Image, useWindowDimensions} from 'react-native';
+import Logo from '../assets/loginLogo.png';
 
 const loginScreen = () => {
+  const {height} = useWindowDimensions();
   return (
-    <View style={styles.container}>
-      <Text>LoginScreen</Text>
-      <Button title="Click me" onPress={() => alert('Button Clicked!')} />
+    <View style={styles.root}>
+      <Image source = {Logo} style ={[styles.logo, {height: height * 0.3}]}
+      resizeMode = "contain" />
     </View>
   );
 };
@@ -13,9 +15,15 @@ const loginScreen = () => {
 export default loginScreen;
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: '70%',
+    maxWidth: 100,
+    maxHeight: 100,
   },
 });
