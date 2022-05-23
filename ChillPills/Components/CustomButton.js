cbc13de8ec69157dd210f0bc0a3ac7af8f889359
T-Wan-Lin <tanwanlin.c2021@gmail.com>
2022-05-23 21:35:17 +0800

    Login UI

--- ChillPills/Components/CustomButton.js
+++ ChillPills/Components/CustomButton.js
@@ -1,18 +1,30 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Pressable } from 'react-native'
 
 
 const CustomButton = () => {
   return (
-    <View>
-      <Text> CustomButton </Text>
+    <View style = {styles.container}>
+      <Text style = {styles.text}> Button </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {},
-    text: {}
+    container: {
+        backgroundColor: '#0095AB',
+        width: '100%',
+        padding: 20,
+        marginVertical: 20,
+        alignItems: 'center',
+        borderRadius: 5,
+
+
+    },
+    text: {
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+    }
 });
 
 export default CustomButton
