8c0a6b7c6ff9d3361e6748eb92e6cec549525ec8
T-Wan-Lin <tanwanlin.c2021@gmail.com>
2022-06-14 15:12:05 +0800

    Pet Screen UI

--- ChillPills/screens/ChoosePetScreen.js
+++ ChillPills/screens/ChoosePetScreen.js
@@ -1,21 +1,47 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet, Input } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet, Input, Image, useWindowDimensions } from 'react-native';
 
 import CustomButton from '../Components/CustomButton';
 import { useNavigation } from '@react-navigation/native';
+import CasperBadge from '../assets/CasperBadge.png';
 
 const ChoosePetScreen = () => {
-  const navigation = useNavigation();
-  const onLogoutPressed = () => {
-    //console.warn("Register");
-    navigation.navigate('Login');
+  const { height } = useWindowDimensions();
+  const onChosen = () => {
+    console.warn("Store user choice, nav to choose interval");
+    
   };
-  return (
-    <View>
-      <Text>ChoosePetScreen is Under Construction</Text>
-      <CustomButton text="Log Out" onPress={onLogoutPressed} />
+return (
+    <View style={styles.root}>
+      <Text> Choose your adorable pet! </Text>
+      <Text> </Text>
+      <Image
+        source={CasperBadge}
+        style={[styles.badge, { height: height * 0.5 }]}
+        resizeMode="contain"
+      />
+      <Text> </Text> 
+      <Text>Casper, the Curious Prince-in-waiting</Text>
+      <Text> </Text> 
+      <CustomButton text="Choose" onPress={onChosen} />
     </View>
   );
 };
 
+
 export default ChoosePetScreen;
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    //justifyContent: 'center',
+    padding: 50,
+    backgroundColor: '#FCF6E2',
+  },
+  badge: {
+    width: '70%',
+    maxWidth: 150,
+    maxHeight: 150,
+  },
+});
