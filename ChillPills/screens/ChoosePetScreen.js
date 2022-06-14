ff9b2585372d5090b609286525d7b02551a638b5
T-Wan-Lin <tanwanlin.c2021@gmail.com>
2022-06-14 15:51:47 +0800

    Choose Pet UI

--- ChillPills/screens/ChoosePetScreen.js
+++ ChillPills/screens/ChoosePetScreen.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, Input, Image, useWindowDimensions } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, useWindowDimensions, ScrollView } from 'react-native';
 
 import CustomButton from '../Components/CustomButton';
 import { useNavigation } from '@react-navigation/native';
 import CasperBadge from '../assets/CasperBadge.png';
+import CamoBadge from '../assets/CamoBadge.png';
+import CuincyBadge from '../assets/CuincyBadge.png'; 
 
 const ChoosePetScreen = () => {
   const { height } = useWindowDimensions();
@@ -12,8 +14,9 @@ const ChoosePetScreen = () => {
     
   };
 return (
+  <ScrollView>
     <View style={styles.root}>
-      <Text> Choose your adorable pet! </Text>
+      <Text style = {styles.title}> Choose your adorable pet! </Text>
       <Text> </Text>
       <Image
         source={CasperBadge}
@@ -21,10 +24,34 @@ return (
         resizeMode="contain"
       />
       <Text> </Text> 
-      <Text>Casper, the Curious Prince-in-waiting</Text>
+      <Text style = {styles.description}>Casper the Curious Prince-in-waiting</Text>
       <Text> </Text> 
       <CustomButton text="Choose" onPress={onChosen} />
+
+      <Text> </Text>
+      <Image
+        source={CamoBadge}
+        style={[styles.badge, { height: height * 0.5 }]}
+        resizeMode="contain"
+      />
+      <Text> </Text> 
+      <Text style = {styles.description}>Camo the Cloudchaser</Text>
+      <Text> </Text> 
+      <CustomButton text="Choose" onPress={onChosen} />
+
+      <Text> </Text>
+      <Image
+        source={CuincyBadge}
+        style={[styles.badge, { height: height * 0.5 }]}
+        resizeMode="contain"
+      />
+      <Text> </Text> 
+      <Text style = {styles.description}>Cuincy the Creative Conceptualizer</Text>
+      <Text> </Text> 
+      <CustomButton text="Choose" onPress={onChosen} />
+
     </View>
+    </ScrollView>
   );
 };
 
@@ -32,6 +59,15 @@ return (
 export default ChoosePetScreen;
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 22,
+    fontFamily: "Futura",
+  },
+  description: {
+    fontFamily: "Futura",
+    fontStyle: 'italic',
+  },
+
   root: {
     flex: 1,
     alignItems: 'center',
